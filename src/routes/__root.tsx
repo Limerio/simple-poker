fb6e3ee340a507037c28364d8679f66996e268f4
Vincent Raoul <vincent.raoul@cyna-it.fr>
2024-10-25 17:00:04 +0200

    fix: remove dev tools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import { AnimatedOutlet } from "@/components/layout/AnimateOutlet";
 import { MainHeader } from "@/components/layout/MainHeader";
 import { createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
@@ -9,7 +8,6 @@ export const Route = createRootRoute({
       <div className="main-background"></div>
       <MainHeader />
       <AnimatedOutlet />
-      <TanStackRouterDevtools position="bottom-right" />
     </>
   ),
 });
