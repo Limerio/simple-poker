ce4ce2e344582b4e8cf8529d7a3e7493b3150c5c
Vincent Raoul <vincent.raoul@cyna-it.fr>
2024-10-25 11:41:59 +0200

    chore: update the index page, the index html file, and the styles.css

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,29 @@
-import { Header } from "@/components/Header";
-import { Button } from "@radix-ui/themes";
-import { createFileRoute } from "@tanstack/react-router";
+import { Button, Heading } from "@radix-ui/themes";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: () => {
+    const navigate = useNavigate();
+    const goToPlayPage = () =>
+      void navigate({
+        to: "/play",
+      });
+
     return (
-      <>
-        <Header />
-        <main>
-          <Button>Get started</Button>
-        </main>
-      </>
+      <main className="fixed flex items-center justify-between p-7 h-full w-full">
+        <div className="flex flex-col items-start gap-5">
+          <Heading size="9" className="text-white">
+            Ready to play?
+          </Heading>
+          <Button
+            className="bg-[var(--red-card)] px-8 py-7 text-2xl font-semibold"
+            size="4"
+            onClick={goToPlayPage}
+          >
+            Yes!
+          </Button>
+        </div>
+      </main>
     );
   },
 });
